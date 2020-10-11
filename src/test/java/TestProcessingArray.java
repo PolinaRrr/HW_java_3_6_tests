034e4f434d5e25b7f0d3a76cc5ca4fe26d0d11ab
PolinaRrr <poniapocalypsisa@gmail.com>
2020-10-12 00:43:14 +0300

    Commit3

--- src/test/java/TestProcessingArray.java
+++ src/test/java/TestProcessingArray.java
@@ -1,3 +1,4 @@
+import com.sun.xml.internal.ws.policy.EffectiveAlternativeSelector;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,8 +16,17 @@ public class TestProcessingArray {
     }
 
     @Test(expected = RuntimeException.class)
-    public void ctestCheckArrayForContents1And4(){
+    public void testSliceArrayAfterLast4Exception(){
         Main.sliceArrayAfterLast4(new int[]{5,7,8,9,3,0});
     }
 
+    @Test
+    public void testCheckArrayForContents1And4() {
+        Assert.assertEquals(Main.checkArrayForContents1And4(new int[]{1,4,1,4,1,4,1,4,4}),true);
+    }
+
+    @Test
+    public void testCheckArrayForContents1And4_1() {
+        Assert.assertEquals(Main.checkArrayForContents1And4(new int[]{1,4,1,6,1,4,1,8,4}),false);
+    }
 }

84c0bda66b79a8f8ac173f23a7f1234ba4778b70
PolinaRrr <poniapocalypsisa@gmail.com>
2020-10-12 00:34:06 +0300

    Commit2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,12 @@ public class Main {
 
         int[] arr = {1, 2, 3, 3, 5, 4, 7, 8, 9, 1, 2, 3, 4, 5, 6, 9, 8, 7};
         int[] arr1 = {1, 1, 4, 1, 4, 1, 4, 1};
-        System.out.println(Arrays.toString(sliceArrayAfter4(arr)));
+        System.out.println(Arrays.toString(sliceArrayAfterLast4(arr)));
 
         System.out.println(checkArrayForContents1And4(arr1));
     }
 
-    public static int[] sliceArrayAfter4(int[] arr) {
+    public static int[] sliceArrayAfterLast4(int[] arr) {
         boolean status = false;
         int[] copyArr = new int[0];
         for (int i = arr.length - 1; i >= 0; i--) {
